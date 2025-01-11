119ad7c678d6659f6cbf6a8d4dd36e4c52444789
Rafiya1207 <rafiya1207@gmail.com>
2025-01-11 15:46:36 +0530

    get user input to app and display

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def calculator():
-	return
+	if request.method == 'POST':
+		user_input = request.form['user_input']
+		return render_template('index.html', user_input=user_input)
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
