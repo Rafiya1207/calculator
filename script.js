b05304cf14edbfa53a795e4e53ca4030f2a9455d
Rafiya1207 <rafiya1207@gmail.com>
2025-01-11 11:56:55 +0530

    add eventListeners to button to display entered input

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let numberPad = document.getElementById('number-pad')
+const displayInput = (button) =>{
+	let inputField = document.getElementsByClassName('screen')[0]
+	inputField.value += button.textContent
+}
 
 const addButtons = (array, parentElement) => {
 	for (const value of array) {
 		let button = document.createElement('button')
 		button.textContent = value
 		button.setAttribute('type', 'button')
+		button.addEventListener('click', ()=>displayInput(button))
 		parentElement.appendChild(button)
 	}
 	return parentElement
 }
 
+let numberPad = document.getElementById('number-pad')
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 const symbols = ['+', '-', '*', '/', '.']
 
